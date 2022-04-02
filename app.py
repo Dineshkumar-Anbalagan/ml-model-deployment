2745c3295d547dd9eb79f172b1d6f0b8009ae055
Dineshkumar-Anbalagan <dinesh2172@outlook.com>
2022-04-02 17:55:14 +0530

    m3

--- app.py
+++ app.py
@@ -5,21 +5,13 @@ app = Flask(__name__)
 
 @app.route("/", methods = ["GET", "POST"])
 def hello():
+    price_pred = ""
     if request.method == "POST":
         carat = request.form["carat"]
         x, y, z = request.form["x"], request.form["y"], request.form["z"]
         price_pred = price.price_prediction(carat, x, y, z)
         
-    return render_template("index.html", mpred = price_pred[0])
-
-# @app.route("/submit", methods = ["POST"])
-# def submit():
-#     # html --> .py
-#     if request.method == "POST":
-#         name = request.form["username"]
-    
-#     # .py --> html
-#     return render_template("submit.html", n = name)
+    return render_template("index.html", mpred = price_pred)
 
 if __name__ == "__main__":
     app.run(debug = True)
